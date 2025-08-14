bdf56a620f0702672a6a78fb59420934fb71eaf4
Jeffrey <562475805lanu@gmail.com>
2025-08-14 15:13:22 +1000

    deleted unwatned codes

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+//https://react-dev-inspector.zthxxx.me/docs/inspector-component ,_ new plug in may delete la
+//import { inspectorServer } from 'react-dev-inspector/vite-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(),],//inspectorServer()
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  }
+});
