627136cacc074fbeb4a7e7c5fd1c916a087cd3f1
Jeffrey <562475805lanu@gmail.com>
2025-08-21 17:16:50 +1000

    Linked to MongoDB

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,11 @@
-import dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import mongoose from "mongoose";
 
 const app = express();
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: "./Server/config.env" });
 
 app.use(
   cors({
@@ -20,21 +18,16 @@ app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 // Routes
 // app.use('/', someRoute);
 
-const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace("<db_password>", process.env.DATABASE_PASSWORD);
 
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
+  .connect(DB)
   .then(() => console.log("DB connection successful"))
   .catch((err) => console.log("Connection ERROR💥:", err));
 
 // Server
 const port = 8000 || process.env.PORT;
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`app running on ${port}...`);
 });
