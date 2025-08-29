d16c30d46a23ac736936e46fa3dca421046d0c7d
Tom <8527295+OfficialTomJ@users.noreply.github.com>
2025-08-29 14:41:49 +1000

    Increase payload limit to 1mb

--- Server/server.js
+++ Server/server.js
@@ -18,7 +18,7 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "10kb" }));
+app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
 app.use(function (req, res, next) {
