402e56386cec443b009a079deed18636cfdf1b1d
Jeffrey <562475805lanu@gmail.com>
2025-08-30 15:22:40 +1000

    Changed home & profile view after login

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Card from "../components/UI/Card";
 
 function Profile() {
   const { user } = useSelector((state) => state.userInfo);
-  const { name } = user;
+  const { name, email, role } = user;
+  console.log(user);
+
+  if (!user) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <Card className="text-center text-slate-70 justify-center items-center m-10">
+      <h1 className="mb-2">Profile</h1>
+
       <p>Welcome back, {name}</p>
-    </div>
+      <p>Email: {email}</p>
+      <p>Role: {role}</p>
+    </Card>
   );
 }
 
