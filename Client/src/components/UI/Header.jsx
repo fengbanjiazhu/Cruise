f7999c959131b0c179981915b8c02b90c4b6c161
Jeffrey <562475805lanu@gmail.com>
2025-09-03 22:52:38 +1000

    Fixed header issue

--- Client/src/components/UI/Header.jsx
+++ Client/src/components/UI/Header.jsx
@@ -1,6 +1,7 @@
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { removeUser } from "../../store/slices/userInfoSlice";
 import { useDispatch, useSelector } from "react-redux";
+import logo from "@/assets/cruise_logo.png";
 
 function Header() {
   const { isLoggedIn } = useSelector((state) => state.userInfo);
@@ -13,36 +14,28 @@ function Header() {
 
   return (
     <>
-      <header className="header ">
-        {/* <img src={SomeLogo} alt="Logo" /> */}
-        {/* <h1>
-          <Link to="/">Cruise</Link>
-        </h1>
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/profile">Profile</NavLink>
-          <NavLink to="/path">Path</NavLink>
-          
-        </h1> */}
+      <header className="header px-4 flex justify-between">
+        <Link to="/">
+          <img className="h-[2.5rem] mx-2" src={logo} alt="Logo" />
+        </Link>
+
         <nav className="align-center">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/path">Path</NavLink>
-            <NavLink to="/register">Register</NavLink>
-            <NavLink to="/admin">Admin</NavLink>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/path">Path</NavLink>
 
-            {isLoggedIn && <NavLink to="/createpath">Create Path</NavLink>}
-            {isLoggedIn && <NavLink to="/review">Reviews</NavLink>}
-            {isLoggedIn && <NavLink to="/profile">Profile</NavLink>}
-                
-            {isLoggedIn && (
-                <a onClick={logout} role="button">
-                Logout
-                </a>
-            )}
+          {isLoggedIn && <NavLink to="/createpath">Create Path</NavLink>}
+          {isLoggedIn && <NavLink to="/review">Reviews</NavLink>}
+          {isLoggedIn && <NavLink to="/profile">Profile</NavLink>}
+          {isLoggedIn && <NavLink to="/admin">Admin</NavLink>}
 
-            {!isLoggedIn && <NavLink to="/login">Login</NavLink>}
-            
-            {/* {!isLoggedIn && <NavLink to="/login">Register</NavLink>} */}
+          {isLoggedIn && (
+            <a onClick={logout} role="button">
+              Logout
+            </a>
+          )}
+
+          {!isLoggedIn && <NavLink to="/login">Login</NavLink>}
+          {!isLoggedIn && <NavLink to="/register">Register</NavLink>}
         </nav>
       </header>
     </>
