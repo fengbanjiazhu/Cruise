7420e5969ab1dff46bee7abe2042d7ba3a548acd
Thomas Johnston <8527295+OfficialTomJ@users.noreply.github.com>
2025-09-23 17:12:14 +1000

    Update allpaths unit tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,3 +1,12 @@
 module.exports = {
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(react-leaflet|@react-leaflet/core|leaflet)/)',
+  ],
 };
