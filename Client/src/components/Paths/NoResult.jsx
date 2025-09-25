22475a3ba41c1627d4c9d755c57ec3fd800fb1b0
Jeffrey <562475805lanu@gmail.com>
2025-09-25 17:08:01 +1000

    Completed path search filter function

---
+++ Client/src/components/Paths/NoResult.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+import { GrDocumentMissing } from "react-icons/gr";
+
+function NoResult({
+  title = "There is no match",
+  message = "Looks like there is no matching results, please try a different condition",
+  children,
+}) {
+  return (
+    <Card className="w-ful h-full m-auto max-w-[900px] rounded-xl bg-white text-[#222] border border-[#ececf0]">
+      <CardHeader>
+        <CardTitle className="flex gap-2">
+          {title} <GrDocumentMissing size={20} />
+        </CardTitle>
+        <CardDescription>{message}</CardDescription>
+        <div></div>
+        <div className="mt-2">{children}</div>
+      </CardHeader>
+    </Card>
+  );
+}
+
+export default NoResult;
--- Client/src/components/Paths/SearchBar.jsx
+++ Client/src/components/Paths/SearchBar.jsx
@@ -6,28 +6,17 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-
-import LocationGetter from "../Search/LocationGetter";
-
-import ProfileSelector from "../Search/ProfileSelector";
-
-import { useState } from "react";
+import SearchForm from "./SearchForm";
 
 function SearchBar() {
-  //   const [open, setOpen] = useState(false);
-
   return (
     <Sheet>
-      <SheetTrigger>Search</SheetTrigger>
-      <SheetContent className="w-[200px] sm:w-[300px] mt-14 rounded-lg pt-20">
+      <SheetTrigger className="border-slate-300">Search</SheetTrigger>
+      <SheetContent className="w-[200px] sm:w-[300px] mt-14 rounded-lg pt-10 ">
         <SheetHeader>
           <SheetTitle>Choose your action</SheetTitle>
           <SheetDescription>
-            <SaerchForm />
+            <SearchForm />
           </SheetDescription>
         </SheetHeader>
       </SheetContent>
@@ -36,33 +25,3 @@ function SearchBar() {
 }
 
 export default SearchBar;
-
-function SaerchForm({ className }) {
-  const [location, setLocation] = useState({ lat: null, lng: null });
-  const [radius, setRadius] = useState(10);
-  const [profile, setProfile] = useState(null);
-
-  return (
-    <div className={`grid items-start gap-6 ${className}`}>
-      <div className="grid gap-3">
-        <Label htmlFor="name">Path Name</Label>
-        <Input id="name" placeholder="Enter path name" />
-      </div>
-      <div className="grid gap-3">
-        <Label htmlFor="duration">Duration (min)</Label>
-        <Input id="duration" />
-      </div>
-
-      <LocationGetter
-        location={location}
-        setLocation={setLocation}
-        radius={radius}
-        setRadius={setRadius}
-      />
-
-      <ProfileSelector profile={profile} setProfile={setProfile} />
-
-      <Button>Apply Condition</Button>
-    </div>
-  );
-}
